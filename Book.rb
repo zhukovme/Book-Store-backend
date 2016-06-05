ebb39f6ce477242f6a2267f3453fd0a9afd3254d
zhukovme <zhukovme@gmail.com>
2016-06-05 23:22:48 +0300

    Fix require, fix delete reuest

--- Book.rb
+++ Book.rb
@@ -1,8 +1,2 @@
 class Book < ActiveRecord::Base
-
-  def to_s
-    "Book id = #{self.id}, title = #{self.title}, author = #{self.author}, year = #{self.year}, publisher = #{self.publisher}"
-  end
-  
-
 end
--- app.rb
+++ app.rb
@@ -1,69 +1,65 @@
 require 'bundler'
 Bundler.require
+require_relative 'Book'
 
 ActiveRecord::Base.establish_connection(
-	:adapter => 'postgresql',
-	:database => 'bookstore'
+  :adapter => 'postgresql',
+  :database => 'bookstore',
+  :username => 'postgres',
+  :password => '1121'
 )
 
 get '/' do
 
-	@book = Book.new
-	@book.title = "The Great Book"
-	@book.author = "Jon Bovi"
-	@book.year = 1999
-	@book.publisher = "Ya"
-	@book.to_json
-
 end
 
 get '/api/books' do
-	Book.all.to_json
+  Book.all.to_json
 end
 
 get '/api/book/:id' do
-	puts params
-	Book.find(params[:id]).to_json
+  puts params
+  Book.find(params[:id]).to_json
 end
 
 post '/api/books' do
-	puts params
-	Book.create({
-		:title => params[:title],
-		:author => params[:author],
-		:year => params[:year],
-		:publisher => params[:publisher]
-	}).to_json
+  puts params
+  Book.create({
+    :title => params[:title],
+    :author => params[:author],
+    :year => params[:year],
+    :publisher => params[:publisher]
+  }).to_json
 end
 
 patch '/api/books/:id' do
-	book_args = { 
-		:title => params[:title], 
-		:author => params[:author], 
-		:year => params[:year], 
-		:publisher => params[:publisher]
-	}
+  book_args = { 
+    :title => params[:title], 
+    :author => params[:author], 
+    :year => params[:year], 
+    :publisher => params[:publisher]
+  }
 
-	@book = Book.find(params[:id])
-	@book.update(book_args)
-	@book.to_json
+  @book = Book.find(params[:id])
+  @book.update(book_args)
+  @book.to_json
 
 end
 
 put '/api/books/:id' do
-	book_args = { 
-		:title => params[:title], 
-		:author => params[:author], 
-		:year => params[:year], 
-		:publisher => params[:publisher]
-	}
+  book_args = { 
+    :title => params[:title], 
+    :author => params[:author], 
+    :year => params[:year], 
+    :publisher => params[:publisher]
+  }
 
-	@book = Book.find(params[:id])
-	@book.update(book_args)
-	@book.to_json
+  @book = Book.find(params[:id])
+  @book.update(book_args)
+  @book.to_json
 
 end
 
 delete '/api/books/:id' do
-	book.destroy(params[:id]).to_json
+  Book.destroy(params[:id]).to_json
 end
